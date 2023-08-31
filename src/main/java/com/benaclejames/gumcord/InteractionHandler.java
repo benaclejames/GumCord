e9c08922c5e9afc1bfc55f2a3a600100ac21f780
benaclejames <benaclejames@gmail.com>
2023-08-31 12:13:47 +0100

    fix(interactions): Handle over 25 options properly

--- src/main/java/com/benaclejames/gumcord/InteractionHandler.java
+++ src/main/java/com/benaclejames/gumcord/InteractionHandler.java
@@ -51,9 +51,15 @@ public class InteractionHandler extends ListenerAdapter {
             {
                 String productId = event.getOption("product_id").getAsString();
                 Role role = event.getOption("role").getAsRole();
+                GumServer server = DynamoHelper.GetServer(event.getGuild());
+
+                // Ensure we don't have 25 roles already linked
+                if (server.getRoles().size() >= 25) {
+                    event.reply("You can't link more than 25 roles (for now). Please unlink a role before using this command").setEphemeral(true).queue();
+                    return;
+                }
 
                 // Check that this role is not already linked to a product
-                GumServer server = DynamoHelper.GetServer(event.getGuild());
                 if (server.getRoles().containsKey(productId)) {
                     event.reply("This product is already linked to a role!").setEphemeral(true).queue();
                     return;
