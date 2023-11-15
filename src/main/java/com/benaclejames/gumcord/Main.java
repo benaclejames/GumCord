5679edd6a79d6441f7a6f4d527538cdb1a88216a
Ben Thomas <benaclejames@gmail.com>
2023-11-15 16:04:57 -0500

    fix: remove privileged message content intent

--- src/main/java/com/benaclejames/gumcord/Main.java
+++ src/main/java/com/benaclejames/gumcord/Main.java
@@ -11,7 +11,6 @@ public class Main {
 
     public static void main(String[] args) throws LoginException {
         jda = JDABuilder.createDefault(args[0])
-                .enableIntents(GatewayIntent.MESSAGE_CONTENT)
                 .addEventListeners(new SetupHandler())
                 .addEventListeners(new InteractionHandler())
                 .build();
